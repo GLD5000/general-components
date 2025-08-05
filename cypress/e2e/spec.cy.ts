61df6ebd23f736ddad136b30d06e2147310ab148
GLD5000 <garethlouisdevlin@gmail.com>
2025-08-05 20:17:22 +0100

    Add Cypress types

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Home Page Tests", () => {
+  it("Loads", () => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("Has Links", () => {
+    cy.visit("http://localhost:3000/");
+
+    cy.get("a h2").should('have.length.greaterThan', 3);
+  });
+});
