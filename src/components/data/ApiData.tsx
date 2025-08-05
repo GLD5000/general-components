27a60d61b29fa40b6feee0c3362ec1ba33531ca9
GLD5000 <garethlouisdevlin@gmail.com>
2025-08-05 17:55:06 +0100

    Update links

--- src/components/data/ApiData.tsx
+++ src/components/data/ApiData.tsx
@@ -6,6 +6,6 @@ export default function ApiData() {
     const apiData = useApiJson();
   return (
    
-    <div>{JSON.stringify(apiData.data)}</div>
+      <div>{apiData.data?.map((tuple, index) => <a target="_blank" style={{color:'unset'}} key={`${tuple[0]}-${index}`} href={tuple[1]}><h2 className="text-black">{tuple[0]}</h2></a>)}</div>
   )
 }
--- src/hooks/useApiJson.tsx
+++ src/hooks/useApiJson.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect, Dispatch, SetStateAction } from "react";
 
 export default function useApiJson(): {
-  data: string[] | null;
+  data: string[][] | null;
   isLoading: boolean;
   refetch: () => void;
 } {
-  const [data, setData] = useState<string[] | null>(null);
+  const [data, setData] = useState<string[][] | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -20,14 +20,19 @@ export default function useApiJson(): {
 }
 
 async function fetchJson(
-  setData: Dispatch<SetStateAction<string[] | null>>,
+  setData: Dispatch<SetStateAction<string[][] | null>>,
   setIsLoading: Dispatch<SetStateAction<boolean>>
 ): Promise<void> {
   try {
     const response = await fetch(`/api/getApiData/`);
-    const jsonData = await response.json() as ApiDataJson;
+    const jsonData = (await response.json()) as ApiDataJson;
 
-    setData(Object.values(jsonData.resources).map(object => object.title));
+    setData(
+      Object.values(jsonData.resources).map((object) => [
+        object.title,
+        object.url,
+      ])
+    );
   } catch (error) {
     console.error("Error fetching JSON files:", error);
   } finally {
@@ -36,74 +41,74 @@ async function fetchJson(
 }
 
 export interface ApiDataJson {
-  id: string
-  site: string
-  slug: string
-  createdAt: string
-  updatedAt: string
-  london_smallest_geography: string
-  descriptionIframe: string
-  author: string
-  author_email: string
-  maintainer: string
-  maintainer_email: string
-  update_frequency: string
-  licence: string
-  description: string
-  tags: string[]
-  topics: string[]
-  resources: Record<string,ApiDataResource>
-  parent: string
-  state: string
-  title: string
-  sharing: string
-  shares: Shares
-  readonly: Readonly
+  id: string;
+  site: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  london_smallest_geography: string;
+  descriptionIframe: string;
+  author: string;
+  author_email: string;
+  maintainer: string;
+  maintainer_email: string;
+  update_frequency: string;
+  licence: string;
+  description: string;
+  tags: string[];
+  topics: string[];
+  resources: Record<string, ApiDataResource>;
+  parent: string;
+  state: string;
+  title: string;
+  sharing: string;
+  shares: Shares;
+  readonly: Readonly;
 }
 
- export interface ApiDataResource {
-  url: string
-  order: number
-  title: string
-  format: string
-  origin: string
-  check_hash: string
-  check_size: number
-  description: string
-  check_mimetype: string
-  london_res_geo: any[]
-  check_timestamp: string
-  check_http_status: number
-  temporal_coverage_to: string
-  temporal_coverage_from: string
+export interface ApiDataResource {
+  url: string;
+  order: number;
+  title: string;
+  format: string;
+  origin: string;
+  check_hash: string;
+  check_size: number;
+  description: string;
+  check_mimetype: string;
+  london_res_geo: any[];
+  check_timestamp: string;
+  check_http_status: number;
+  temporal_coverage_to: string;
+  temporal_coverage_from: string;
 }
 
 export interface Shares {
-  orgs: any[]
-  users: any[]
+  orgs: any[];
+  users: any[];
 }
 
 export interface Readonly {
-  parent: Parent
-  licence: Licence
-  topics: Record<string,Topic>
+  parent: Parent;
+  licence: Licence;
+  topics: Record<string, Topic>;
 }
 
 export interface Parent {
-  id: string
-  slug: string
-  title: string
-  img: string
+  id: string;
+  slug: string;
+  title: string;
+  img: string;
 }
 
 export interface Licence {
-  title: string
-  url: string
-  is_okd_compliant: boolean
+  title: string;
+  url: string;
+  is_okd_compliant: boolean;
 }
 
 export interface Topic {
-  img: string
-  slug: string
-  title: string
+  img: string;
+  slug: string;
+  title: string;
 }
